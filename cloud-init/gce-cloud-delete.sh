be8d933a6874ef25ad60d4ad60499eb624897fe2
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2020-01-03 19:01:34 +0530

    Update gce-cloud-delete.sh

--- cloud-init/gce-cloud-delete.sh
+++ cloud-init/gce-cloud-delete.sh
@@ -5,22 +5,13 @@ export CLOUD_TYPE="gce"
 export CLOUD_REGION="us-central1"
 export CLOUD_ZONE="us-central1-c"
 
-export BASTION_HOST_NAME="bastion-host-01"
-export BASTION_MACHINE_TYPE="f1-micro"
-export BASTION_NETWORK_TIER="STANDARD"
-export BASTION_PREEMPTIBLE="Yes"
-export BASTION_TAGS="bastion-host,http-server,https-server"
-export BASTION_IMAGE="ubuntu-1804-bionic-v20191211"
-export BASTION_IMAGE_PROJECT="ubuntu-os-cloud"
-export BASTION_BOOT_DISK_SIZE="10GB"
-export BASTION_BOOT_DISK_TYPE="pd-standard"
-export BASTION_LABELS="type=bastion-host"
-
 echo
 echo
 echo "Removing cloud environemnt for DigiKube.  Cloud provider: gce."
 echo
 
+##########################################################
+#Get cloud project details
 export CLOUD_PROJECT="$(gcloud info |tr -d '[]' | awk '/project:/ {print $2}')"
 if [ $? -gt 0 ]; then
 	echo "Unable to get the project details for DigiKube.  Exiting the DigiKube delete."
@@ -35,8 +26,10 @@ else
 	echo "Deleting DigiKube resources from cloud project.  Cloud project id: ${CLOUD_PROJECT}."
 fi
 
+##########################################################
 #Delete the Bastion Host for DigiKube
 echo "Attempting to delete bastion host for Digikube.  Bastion host name: ${BASTION_HOST_NAME}."
+export BASTION_HOST_NAME="bastion-host-01"
 export BASTION_HOST_ZONE=$(gcloud compute instances list --filter="name=${BASTION_HOST_NAME}" --format="value(zone)")
 if [ $? -gt 0 ]; then
 	echo "Unable to get the bastion host or its zone details.  Exiting the DigiKube delete."
@@ -64,8 +57,27 @@ else
   fi
 fi
 
-#Delete the network for DigiKube
+###########################################################
+#Delete firewall rule for bastion host
 export CLOUD_SUBNET="${CLOUD_PROJECT}-vpc"
+export BASTION_HOST_FIREWALL_RULE_NAME="${CLOUD_SUBNET}-allow-bastion-ssh"
+echo "Attempting to delete firewall rule for bastion host: ${BASTION_HOST_FIREWALL_RULE_NAME}"
+if [ -z $(gcloud compute firewall-rules list --filter=name=${BASTION_HOST_FIREWALL_RULE_NAME} --format="value(name)") ]; then
+	echo "No firewall rule available with the name ${BASTION_HOST_FIREWALL_RULE_NAME}.  Skipping firewall rule deletion."
+else
+	gcloud -q compute firewall-rules delete ${BASTION_HOST_FIREWALL_RULE_NAME}
+	if [ $? -gt 0 ]; then
+    		#Unknown error while deleting the firewall rule.
+    		echo "Unable to delete firewall rule for bastion host.  Exiting the DigiKube delete."
+    		echo "Manually review and delete DigiKube cloud resources."
+    		exit 1
+  	else
+    		echo "Deleted the firewall rule for bastion host: ${BASTION_HOST_FIREWALL_RULE_NAME}."
+	fi
+fi
+
+###########################################################
+#Delete the network for DigiKube
 echo "Attempting to delete network for Digikube.  Network name: ${CLOUD_SUBNET}."
 if [ -z $(gcloud compute networks list --filter=name=${CLOUD_SUBNET} --format="value(name)") ]; then
   echo "No network available with the name ${CLOUD_SUBNET}.  Skipping network deletion."

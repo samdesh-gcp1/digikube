14ec0625467ccf286a571334d78c63a7990e9d5b
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2020-01-03 17:36:45 +0530

    Update gce-cloud-delete.sh

--- cloud-init/gce-cloud-delete.sh
+++ cloud-init/gce-cloud-delete.sh
@@ -23,12 +23,12 @@ echo
 
 export CLOUD_PROJECT="$(gcloud info |tr -d '[]' | awk '/project:/ {print $2}')"
 if [ $? -gt 0 ]; then
-	echo "Unable to get the project details for DigiKube.  Exiting the DigiKube delete.
+	echo "Unable to get the project details for DigiKube.  Exiting the DigiKube delete."
 	echo "Manually review and delete DigiKube cloud resources."
 	exit 1
 fi
 if [ -z ${CLOUD_PROJECT} ]; then
-	echo "Unable to get the project details for DigiKube.  Exiting the DigiKube delete.
+	echo "Unable to get the project details for DigiKube.  Exiting the DigiKube delete."
 	echo "Manually review and delete DigiKube cloud resources."
 	exit 1
 else
@@ -37,13 +37,26 @@ fi
 
 #Delete the Bastion Host for DigiKube
 echo "Attempting to delete bastion host for Digikube.  Bastion host name: ${BASTION_HOST_NAME}."
-if [ -z $(gcloud compute instances list --filter=name=${BASTION_HOST_NAME} --format="value(name)") ]; then
+export BASTION_HOST_ZONE=$(gcloud compute instances list --filter="name=${BASTION_HOST_NAME}" --format="value(zone)"
+if [ $? -gt 0 ]; then
+	echo "Unable to get the bastion host or its zone details.  Exiting the DigiKube delete."
+	echo "Manually review and delete DigiKube cloud resources."
+	exit 1
+fi
+if [ -z ${BASTION_HOST_ZONE} ]; then
+	echo "Unable to get the bastion host or its zone details.  Exiting the DigiKube delete."
+	echo "Manually review and delete DigiKube cloud resources."
+	exit 1
+else
+	echo "Deleting bastion host in zone: ${BASTION_HOST_ZONE}."
+fi
+if [ -z $(gcloud compute instances list --filter="name=${BASTION_HOST_NAME}" --format="value(name)") ]; then
   echo "No bastion host available with the name ${BASTION_HOST_NAME}.  Skipping bastion host deletion."
 else
-  gcloud --quiet compute instances delete ${BASTION_HOST_NAME}
+  gcloud --quiet compute instances delete ${BASTION_HOST_NAME} --zone=${BASTION_HOST_ZONE}
   if [ $? -gt 0 ]; then
     #Unknown error while deleting the bastion host.
-    echo "Unable to delete bastion host for DigiKube.  Exiting the DigiKube delete.
+    echo "Unable to delete bastion host for DigiKube.  Exiting the DigiKube delete."
     echo "Manually review and delete DigiKube cloud resources."
     exit 1
   else
@@ -60,7 +73,7 @@ else
   gcloud --quiet compute networks delete ${CLOUD_SUBNET}
   if [ $? -gt 0 ]; then
     #Unknown error while deleting the network.
-    echo "Unable to delete network for DigiKube.  Exiting the DigiKube delete.
+    echo "Unable to delete network for DigiKube.  Exiting the DigiKube delete."
     echo "Manually review and delete DigiKube cloud resources."
     exit 1
   else

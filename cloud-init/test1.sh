2b6288e6e921448eb3974632a0956a0bee53f4a7
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2020-01-06 15:05:45 +0530

    Update test1.sh

--- cloud-init/test1.sh
+++ cloud-init/test1.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "This is test"
-echo "-- metadata='abcd'"
+echo "-- metadata='${wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/private/master/cloud-init/test1.sh?token=AN6W6B7QJK3POT2LQMWDFLS6CL7VY | bash }'"

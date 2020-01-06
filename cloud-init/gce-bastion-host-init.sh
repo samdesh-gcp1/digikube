9c83a71d2eb6ebca3da89c5ae27e3babb449a0db
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-06 20:50:02 +0530

    Update gce-bastion-host-init.sh

--- cloud-init/gce-bastion-host-init.sh
+++ cloud-init/gce-bastion-host-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Init script
-su - samdesh_gcp1 --preserve-environment  -c "cd /home/samdesh_gcp1/"
-su - samdesh_gcp1 --preserve-environment  -c "touch last_run.txt"
-su - samdesh_gcp1 --preserve-environment  -c "date >> last_run.txt"
+cd /home/samdesh_gcp1/
+touch last_run.txt
+date >> last_run.txt

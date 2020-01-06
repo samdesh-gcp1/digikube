0f65d69244a1809b790c127cf02f9dceac930e1c
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-06 17:25:20 +0530

    Update test1.sh

--- cloud-init/test1.sh
+++ cloud-init/test1.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 echo "This is test"
-echo "-- metadata=`${wget -q -O - https://github.com/samdesh-gcp1/digikube/raw/master/cloud-init/gce-bastion-host-init.sh }`"
+echo "-- metadata='${wget -q -O - https://github.com/samdesh-gcp1/digikube/raw/master/cloud-init/gce-bastion-host-init.sh }'"

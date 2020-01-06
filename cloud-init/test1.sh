6ec74d3ae8aebb47100c3db03b255fb01df504fd
samdesh-gcp1 <58552071+samdesh-gcp1@users.noreply.github.com>
2020-01-06 15:08:20 +0530

    Update test1.sh

--- cloud-init/test1.sh
+++ cloud-init/test1.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "This is test"
-echo "-- metadata='${wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/private/master/cloud-init/gce-bastion-host-init.sh?token=AN6W6B2YJWV4CB6M46HX7YS6CL74M | bash }'"
+echo "-- metadata='$(wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/private/master/cloud-init/gce-bastion-host-init.sh?token=AN6W6B2YJWV4CB6M46HX7YS6CL74M | bash )'"

8f366e206472114dc6fbe90532788683baa37909
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-07 19:50:37 +0530

    Create download-script.sh

---
+++ utility/download-script.sh
@@ -0,0 +1,13 @@
+
+
+FILE_PATH="/tmp/"
+FILE_NAME=""
+
+if [ $# -lt 1 ]; then
+   echo "No file specified for download."
+   exit 1
+else
+   FILE_NAME="$FILE_PATHdigikube-$RANDOM"
+   wget -q -O 
+   
+fi

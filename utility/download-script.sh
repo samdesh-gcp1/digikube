23583e738af068776eff1236f86973389e704192
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-07 20:05:48 +0530

    Update download-script.sh

--- utility/download-script.sh
+++ utility/download-script.sh
@@ -1,13 +1,13 @@
 
 
-FILE_PATH="/tmp/"
-FILE_NAME=""
+DEST_FILE_PATH="/tmp/"
+DEST_FILE_NAME=""
 
 if [ $# -lt 1 ]; then
    echo "No file specified for download."
    exit 1
 else
-   FILE_NAME="$FILE_PATHdigikube-$RANDOM"
-   wget -q -O 
-   
+   SOURCE_URL=$1   #should check the format
+   DEST_FILE_NAME="${DEST_FILE_PATH}digikube-${RANDOM}"
+   wget -q --no-cache -O $DEST_FILE_NAME - $SOURCE_URL
 fi

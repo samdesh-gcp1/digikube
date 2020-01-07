456cfe67df12f79a4524aae4b0369b8230ccdb7f
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-07 18:52:36 +0530

    Update gce-bastion-host-init-shell.sh

--- cloud-init/gce-bastion-host-init-shell.sh
+++ cloud-init/gce-bastion-host-init-shell.sh
@@ -14,6 +14,13 @@ else
   fi
 fi
 
-INIT_SCRIPT_SOURCE="$(wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/digikube/master/cloud-init/gce-bastion-host-init.sh | bash)"
+#INIT_SCRIPT_SOURCE="$(wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/digikube/master/cloud-init/gce-bastion-host-init.sh | bash)"
+#su - $(DIGIKUBE_CLOUD_ADMIN) --preserve-environment -c "$INIT_SCRIPT_SOURCE"
 
-su - $(DIGIKUBE_CLOUD_ADMIN) --preserve-environment -c "$INIT_SCRIPT_SOURCE"
+INIT_SCRIPT_SOURCE="wget -q -O /tmp/gce-bastion-host-init.sh - https://raw.githubusercontent.com/samdesh-gcp1/digikube/master/cloud-init/gce-bastion-host-init.sh"
+
+su - $DIGIKUBE_CLOUD_ADMIN --preserve-environment -c "$INIT_SCRIPT_SOURCE"
+
+su - $DIGIKUBE_CLOUD_ADMIN --preserve-environment -c "chmod +x /tmp/gce-bastion-host-init.sh"
+
+su - $DIGIKUBE_CLOUD_ADMIN --preserve-environment -c "/tmp/gce-bastion-host-init.sh"

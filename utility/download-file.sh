54feb322c674150b6db6eaa62f0590f4e863bf02
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-07 20:21:56 +0530

    Update and rename download-script.sh to download-file.sh

---
+++ utility/download-file.sh
@@ -0,0 +1,21 @@
+function download-file {
+        
+   local dest_file_path="/tmp/"
+   local dest_file_name=""
+
+   if [ $# -lt 2 ]; then
+      echo "Insufficient parameters provided."
+      exit 1
+   else
+      local source_url=$1   #should check the format
+      local dest_file_name="${dest_file_path}digikube-${RANDOM}"
+      wget -q --no-cache -O $dest_file_name - $source_url
+      if [ $? -eq 0 ]; then
+         local  __resultvar=$2
+         eval $__resultvar="'$dest_file_name'"
+      else
+         echo "Error while downloading file."
+         exit 1
+      fi
+   fi
+}

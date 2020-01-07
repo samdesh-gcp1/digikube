583b14005e2937af367b486150122ee31d26cdfc
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-07 17:26:58 +0530

    Update gce-bastion-host-init-shell.sh

--- cloud-init/gce-bastion-host-init-shell.sh
+++ cloud-init/gce-bastion-host-init-shell.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # Init script
 
+#Do not change the bellow line -- will be replaced at runtime
 #<placeholder for digikube admin user name>
 
 if [ $# -gt 0 ]; then
   DIGIKUBE_CLOUD_ADMIN=$1
 else
-  DIGIKUBE_CLOUD_ADMIN=$(whoami)
+  if [ -z $DIGIKUBE_CLOUD_ADMIN ]; then
+    DIGIKUBE_CLOUD_ADMIN=$(whoami)
+  else
+    DIGIKUBE_CLOUD_ADMIN=$DIGIKUBE_CLOUD_ADMIN
+  fi
 fi
 
 INIT_SCRIPT_SOURCE="$(wget -q -O - https://raw.githubusercontent.com/samdesh-gcp1/digikube/master/cloud-init/gce-bastion-host-init.sh | bash)"

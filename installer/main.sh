e9c6829c15ec3c52f203e78f41bff33755833e45
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 18:49:18 +0530

    Update main.sh

--- installer/main.sh
+++ installer/main.sh
@@ -1,4 +1,15 @@
-#
+#!/bin/bash
+# Main installer script
 
-touch ~/digikube-logs/digikube-installer.log
-date >> ~/digikube-logs/digikube-installer.log
+BASE_DIR=~/
+
+LOG_DIR=${BASE_DIR}digikube-logs/
+INSTALLER_LOG=${LOG_DIR}digikube-installer.log
+
+touch $INSTALLER_LOG
+date >> $INSTALLER_LOG
+
+DIGI_DIR=${BASE_DIR}digikube/
+KUBECTL_INSTALLER=${DIGI_DIR}installer/kubectl.sh
+
+${KUBECTL_INSTALLER}

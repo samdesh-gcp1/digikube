86fddc2a5f0abbf32a2ec8f53f9032c9b12dbb81
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 17:53:30 +0530

    Update gce-bastion-host-init.sh

--- cloud-init/gce-bastion-host-init.sh
+++ cloud-init/gce-bastion-host-init.sh
@@ -2,48 +2,65 @@
 # Init script
 
 cd ~/
-touch ~/digikube-init.log
+mkdir ~/digikube-logs
+touch ~/digikube-logs/digikube-init.log
+LOG_FILE=~/digikube-logs/digikube-installer.log
+
+LOG_DIR=~/digikube-logs/
+if [[ -d $LOG_DIR ]]; then
+
+else
+	mkdir $LOG_DIR
+fi
+INIT_LOG=~/$LOG_DIR/digikube-init.log
+INSTALLER_LOG=~/$LOG_DIR/digikube-installer.log
 date >> ~/digikube-init.log
 
-if [[ -d ~/digikube/ ]]; then
-	echo "Dikiguke source directory exists. Refreshing." >> ~/digikube-init.log
-	cd digikube/
+GIT_REPO_URL="https://github.com/samdesh-gcp1/digikube.git"
+
+BASE_DIR=~/
+DIGI_DIR=~/digikube/
+if [[ -d $DIGI_DIR ]]; then
+	echo "Dikiguke source directory exists. Refreshing." >> $INIT_LOG
+	cd $DIGI_DIR
 	git init
 	if [ $? -gt 0 ]; then
-		echo "Error while refreshing dikiguke source directory. Aborting." >> ~/digikube-init.log
+		echo "Error while refreshing dikiguke source directory. Aborting." >> $INIT_LOG
 		exit 1
 	else
-		echo "Refreshed dikiguke source directory." >> ~/digikube-init.log
+		echo "Refreshed dikiguke source directory." >> $INIT_LOG
+		cd $DIGI_DIR
 		git pull
 		if [ $? -gt 0 ]; then
-			echo "Error while pulling dikiguke source directory. Aborting." >> ~/digikube-init.log
+			echo "Error while pulling dikiguke source directory. Aborting." >> $INIT_LOG
 			exit 1
 		else
-			echo "Pulled dikiguke source directory." >> ~/digikube-init.log
+			echo "Pulled dikiguke source directory." >> $INIT_LOG
 		fi	
 	fi	
 else
-	echo "Digikube source directory not available. Clonning" >> ~/digikube-init.log
-	git clone -o digikube-source https://github.com/samdesh-gcp1/digikube.git
+	echo "Digikube source directory not available. Clonning" >> $INIT_LOG
+	cd $BASE_DIR
+	git clone -o digikube-source $GIT_REPO_URL
 	if [ $? -gt 0 ]; then
-		echo "Error while clonning dikiguke source directory. Aborting." >> ~/digikube-init.log
+		echo "Error while clonning dikiguke source directory. Aborting." >> $INIT_LOG
 		exit 1
 	else
-		echo "Clonned dikiguke source directory." >> ~/digikube-init.log
+		echo "Clonned dikiguke source directory." >> $INIT_LOG
 	fi	
 fi
 
-if [[ -e ~/digikube/installer/main.sh ]]; then
-	echo "Starting digikube main installation." >> ~/digikube-init.log
-	~/digikube/installer/main.sh
+if [[ -e "$DIGI_DIR"installer/main.sh ]]; then
+	echo "Starting digikube main installation." >> $INIT_LOG
+	"$DIGI_DIR"installer/main.sh
 	if [[ $? -gt 0 ]]; then
-		echo "Error during Digikube installation.  Please check installation log for details." >> ~/digikube-init.log
+		echo "Error during Digikube installation.  Please check installation log for details." >> $INIT_LOG
 		exit 1
 	else
-		echo "Completed Digikube installation.  Please check installation log for details." >> ~/digikube-init.log
+		echo "Completed Digikube installation.  Please check installation log for details." >> $INIT_LOG
 	fi
 else
-	echo "Digikube main installer not found. Aborting." >> ~/digikube-init.log
+	echo "Digikube main installer not found. Aborting." >> $INIT_LOG
 	exit 1
 fi
 

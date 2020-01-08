32fcec2efa0b22947097fb7750d391f12e53a3dc
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 19:54:58 +0530

    Update kubectl.sh

--- installer/kubectl.sh
+++ installer/kubectl.sh
@@ -6,10 +6,21 @@ LOG_DIR=${BASE_DIR}digikube-logs/
 INSTALLER_LOG=${LOG_DIR}digikube-installer.log
 DIGI_DIR=${BASE_DIR}digikube/
 
-KUBECTL_VERSION="1.15"
+KUBECTL_TARGET_VERSION="1.15+"
+KUBECTL_CUR_PATH=$(which kubectl)
 
-if [[ -z $(which kubectl) ]]; then
-        
+if [[ -z ${KUBECTL_CUR_PATH} ]]; then
+    echo "Kubectl binory already available. Location: ${KUBECTL_CUR_PATH}"
+    eval $(parse_yaml <( kubectl version -o yaml ) "KUBECTL_CUR_")
+    KUBECTL_CUR_VERSION=$KUBECTL_CUR_clientVersion_major.$KUBECTL_CUR_clientVersion_minor
+    if [[ "$KUBECTL_CUR_VERSION" = "$KUBECTL_TARGET_VERSION" ]]; then
+        echo "Kubectl version is ${KUBECTL_CUR_VERSION}.  Skipping kubectl installation."
+        exit 0
+    else
+        echo "Kubectl version is ${KUBECTL_CUR_VERSION}.  Required version is ${KUBECTL_TARGET_VERSION}.  Aborting kubectl installation.  Please remove the old version and rerun the installation."
+        exit 1
+    fi
 else
-    #Kubectl not available.
+    #Kubectl not available.  Download and install
     
+fi

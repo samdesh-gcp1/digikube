f465ee6e2edde198ab8bc81185f5a97a04f2a7cd
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 14:46:09 +0530

    Update gce-bastion-host-init.sh

--- cloud-init/gce-bastion-host-init.sh
+++ cloud-init/gce-bastion-host-init.sh
@@ -2,40 +2,41 @@
 # Init script
 
 cd ~/
-touch digikube-init.log
-date >> digikube-init.log
+touch ~/digikube-init.log
+date >> ~/digikube-init.log
 
 if [[ -d "digikube/" ]]; then
-	echo "Dikiguke source directory exists. Refreshing." >> digikube-init.log
+	echo "Dikiguke source directory exists. Refreshing." >> ~/digikube-init.log
 	cd digikube/
 	git init
 	if [ $? -gt 0 ]; then
-		echo "Error while refreshing dikiguke source directory. Aborting." >> digikube-init.log
+		echo "Error while refreshing dikiguke source directory. Aborting." >> ~/digikube-init.log
 		exit 1
 	else
 		echo "Refreshed dikiguke source directory." >> digikube-init.log
+		git pull
 		if [ $? -gt 0 ]; then
-			echo "Error while pulling dikiguke source directory. Aborting." >> digikube-init.log
+			echo "Error while pulling dikiguke source directory. Aborting." >> ~/digikube-init.log
 			exit 1
 		else
-			echo "Pulled dikiguke source directory." >> digikube-init.log
+			echo "Pulled dikiguke source directory." >> ~/digikube-init.log
 		fi	
 	fi	
 else
-	echo "Digikube source directory not available. Clonning" >> digikube-init.log
+	echo "Digikube source directory not available. Clonning" >> ~/digikube-init.log
 	git clone -o digikube-source https://github.com/samdesh-gcp1/digikube.git
 	if [ $? -gt 0 ]; then
-		echo "Error while clonning dikiguke source directory. Aborting." >> digikube-init.log
+		echo "Error while clonning dikiguke source directory. Aborting." >> ~/digikube-init.log
 		exit 1
 	else
-		echo "Clonned dikiguke source directory." >> digikube-init.log
+		echo "Clonned dikiguke source directory." >> ~/digikube-init.log
 	fi	
 fi
 
 if [[ -f "~/digikube/installer/main.sh" ]]; then
-	echo "Starting digikube main installation." >> digikube-init.log
+	echo "Starting digikube main installation." >> ~/digikube-init.log
 else
-	echo "Digikube main installer not found. Aborting." >> digikube-init.log
+	echo "Digikube main installer not found. Aborting." >> ~/digikube-init.log
 	exit 1
 fi
 

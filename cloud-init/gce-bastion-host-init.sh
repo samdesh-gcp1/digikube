a3e1787b8d6a2228f4feae49a160d0273ae052f0
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 14:32:44 +0530

    Update gce-bastion-host-init.sh

--- cloud-init/gce-bastion-host-init.sh
+++ cloud-init/gce-bastion-host-init.sh
@@ -9,7 +9,28 @@ if [[ -d "digikube/" ]]; then
 	echo "Dikiguke source directory exists. Refreshing." >> digikube-init.log
 	cd digikube/
 	git init
+	if [ $? -gt 0 ]; then
+		echo "Error while refreshing dikiguke source directory. Aborting." >> digikube-init.log
+		exit 1
+	else
+		echo "Refreshed dikiguke source directory." >> digikube-init.log
+	fi	
 else
 	echo "Digikube source directory not available. Clonning" >> digikube-init.log
 	git clone -o digikube-source https://github.com/samdesh-gcp1/digikube.git
+	if [ $? -gt 0 ]; then
+		echo "Error while clonning dikiguke source directory. Aborting." >> digikube-init.log
+		exit 1
+	else
+		echo "Clonned dikiguke source directory." >> digikube-init.log
+	fi	
 fi
+
+if [[ -f "~/digikube/installer/main.sh ]]; then
+	echo "Starting digikube main installation." >> digikube-init.log
+else
+	echo "Digikube main installer not found. Aborting." >> digikube-init.log
+	exit 1
+fi
+
+

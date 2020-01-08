8b6c69fbfa1eab4277a5f8c49b418ceb5f2773e7
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-08 20:32:48 +0530

    Update download-file.sh

--- utility/download-file.sh
+++ utility/download-file.sh
@@ -9,7 +9,9 @@ function download_file {
       exit 1
    else
       local source_url=$1   #should check the format
+      echo $source_url
       local dest_file_name="${dest_file_path}digikube-${RANDOM}"
+      echo $dest_file_name
       wget -q --no-cache -O $dest_file_name - $source_url
       if [ $? -eq 0 ]; then
          local  __resultvar=$2

f79bf2e966326f906bf079d7b9a4f4fafbb6c089
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-09 13:01:46 +0530

    Update kubectl.sh

--- installer/kubectl.sh
+++ installer/kubectl.sh
@@ -2,9 +2,6 @@
 # Main installer script
 
 BASE_DIR=~/
-
-echo "1"
-
 LOG_DIR=${BASE_DIR}digikube-logs/
 INSTALLER_LOG=${LOG_DIR}digikube-installer.log
 DIGI_DIR=${BASE_DIR}digikube/
@@ -12,21 +9,13 @@ DIGI_DIR=${BASE_DIR}digikube/
 . ${DIGI_DIR}utility/download-file.sh
 . ${DIGI_DIR}utility/parse-yaml.sh
 
-echo "2"
-
 KUBECTL_TARGET_VERSION="1.15"
 KUBECTL_BINARY_URL="https://storage.googleapis.com/kubernetes-release/release/v$KUBECTL_TARGET_VERSION.0/bin/linux/amd64/kubectl"
 
-echo "3"
-
 KUBECTL_CUR_PATH=$(which kubectl)
-
-echo "4 $KUBECTL_CUR_PATH"
 
 if [[ -z ${KUBECTL_CUR_PATH} ]]; then
     #Kubectl not available.  Download and install
-    echo "9"
-    
     download_file $KUBECTL_BINARY_URL kubectl_binary
     
     if [[ -z ${kubectl_binary} ]]; then
@@ -39,17 +28,12 @@ if [[ -z ${KUBECTL_CUR_PATH} ]]; then
     
 else
     echo "Kubectl binary already available. Location: ${KUBECTL_CUR_PATH}"
-
-    echo "5"
-
     eval $(parse_yaml <( kubectl version -o yaml ) "KUBECTL_CUR_")
     f=$KUBECTL_CUR_clientVersion_minor
     t="+"
     s=""
     [ "${f%$t*}" != "$f" ] && n="${f%$t*}$s${f#*$t}"
     KUBECTL_CUR_clientVersion_minor=$n
-
-    echo "6"
 
     KUBECTL_CUR_VERSION=$KUBECTL_CUR_clientVersion_major.$KUBECTL_CUR_clientVersion_minor
     if [[ "$KUBECTL_CUR_VERSION" = "$KUBECTL_TARGET_VERSION" ]]; then

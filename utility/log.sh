eaaccda10f264e218f1c40b005e8bfd27cef51dc
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-09 18:24:04 +0530

    Update log.sh

--- utility/log.sh
+++ utility/log.sh
@@ -11,7 +11,7 @@ installer_log=${log_dir}digikube-installer.log
 . ${digi_dir}utility/general.sh
 eval $(parse_yaml $log_config )
 echo "logConfig_installer_logLevel > ${logConfig_installer_logLevel}"
-echo "logConfig_installer_logEcho > $logConfig_installer_logEcho"
+echo "logConfig_installer_logEcho > ${logConfig_installer_logEcho}"
 
 #Critical=4
 #Error=3
@@ -34,20 +34,20 @@ function log_it {
     option="${2}"
     case ${option} in 
 	
-	    "init")         if [[ $log-config_init_log-level -lt $3 ]]; then
+	    "init")         if [[ ${logConfig_init_logLevel -lt $3 ]]; then
 							log_msg="$(date) : $1 : $3 : $4 : $5"
-							echo $log_msg >> ${init_log}
-							if [[ $log-config_init_log-echo == "on" ]]; then
-								echo $log_msg
+							echo ${log_msg} >> ${init_log}
+							if [[ "${logConfig_init_logEcho}" = "on" ]]; then
+								echo ${log_msg}
 							fi
 						fi
                         ;;
 						
-  	    "installer")	if [[ $log-config_installer_log-level -lt $3 ]]; then
+  	    "installer")	if [[ ${logConfig_installer_logLevel} -lt $3 ]]; then
 							log_msg="$(date) : $1 : $3 : $4 : $5"
-							echo $log_msg >> ${installer_log}
-							if [[ $log-config_installer_log-echo == "on" ]]; then
-								echo $log_msg
+							echo ${log_msg} >> ${installer_log}
+							if [[ "${logConfig_installer_logEcho}" = "on" ]]; then
+								echo ${log_msg}
 							fi
 						fi
                         ;;

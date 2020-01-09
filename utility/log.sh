79c92d925852cb73eeddfb6d7f4a58dd7e31f677
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-09 18:36:53 +0530

    Update log.sh

--- utility/log.sh
+++ utility/log.sh
@@ -9,7 +9,7 @@ init_log=${log_dir}digikube-init.log
 installer_log=${log_dir}digikube-installer.log
 
 . ${digi_dir}utility/general.sh
-eval $(parse_yaml $log_config )
+eval $(parse_yaml ${log_config} )
 echo "logConfig_installer_logLevel > ${logConfig_installer_logLevel}"
 echo "logConfig_installer_logEcho > ${logConfig_installer_logEcho}"
 
@@ -24,37 +24,38 @@ function log_it {
     
     local __function_name="utility/log_it"
     
-    echo "inside $__function_name"
+    echo "inside ${__function_name}"
     
     if [ $# -lt 5 ]; then
-        echo "Error: ($__function_name): Insufficient arguments provided: $1"
+        echo "Error: ${__function_name} : Insufficient arguments provided: $1"
         exit 0      #Exit without error
     fi
 
     option="${2}"
     case ${option} in 
 	
-	    "init")			if [[ ${logConfig_init_logLevel -lt $3 ]]; then
-							log_msg="$(date) : $1 : $3 : $4 : $5"
-							echo ${log_msg} >> ${init_log}
-							if [[ "${logConfig_init_logEcho}" = "on" ]]; then
-								echo ${log_msg}
-							fi
-						fi
-                        ;;
+	    "init")
+			if [[ ${logConfig_init_logLevel -lt $3 ]]; then
+				log_msg="$(date) : $1 : $3 : $4 : $5"
+				echo ${log_msg} >> ${init_log}
+				if [[ "${logConfig_init_logEcho}" = "on" ]]; then
+					echo ${log_msg}
+				fi
+			fi
+            ;;
 						
-  	    "installer")	if [[ ${logConfig_installer_logLevel} -lt $3 ]]; then
-							log_msg="$(date) : $1 : $3 : $4 : $5"
-							echo ${log_msg} >> ${installer_log}
-							if [[ "${logConfig_installer_logEcho}" = "on" ]]; then
-								echo ${log_msg}
-							fi
-						fi
-                        ;;
+  	    "installer")
+			if [[ ${logConfig_installer_logLevel} -lt $3 ]]; then
+				log_msg="$(date) : $1 : $3 : $4 : $5"
+				echo ${log_msg} >> ${installer_log}
+				if [[ "${logConfig_installer_logEcho}" = "on" ]]; then
+					echo ${log_msg}
+				fi
+			fi
+            ;;
 	
-	    *)	            echo "Error: ($__function_name): Unkwonk log type: ${option}"
-		                exit 1
-		                
+	    *)
+			echo "Error: ${__function_name} : Unkwonk log type: ${option}"
+		    exit 1            
     esac
-	
 }

d1f184e26377b96e12f61f540cff2fcfab5b9d62
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-09 19:04:01 +0530

    Update log.sh

--- utility/log.sh
+++ utility/log.sh
@@ -13,29 +13,19 @@ eval $(parse_yaml ${log_config} )
 echo "logConfig_installer_logLevel > ${logConfig_installer_logLevel}"
 echo "logConfig_installer_logEcho > ${logConfig_installer_logEcho}"
 
-#Critical=4
-#Error=3
-#Warning=2
-#Information=1
-#Debug=0
-
 echo "test"
 
 log_it() {
-    
-    local __function_name="utility/log_it"
-    
-    echo "inside ${__function_name}"
-    
-    if [ $# -lt 5 ]; then
-        echo "Error: ${__function_name} : Insufficient arguments provided: $1"
-        exit 0      
-    fi
 
-    option=${2}
+	local __function_name="utility/log_it"
+	echo "inside ${__function_name}"
+	if [[ $# -lt 5 ]]; then
+		echo "Error: ${__function_name} : Insufficient arguments provided: ${1}"
+		exit 0
+	fi
+	option=${2}
 	if [[ "${option}" = "init" ]]; then
 		if [[ ${logConfig_init_logLevel -lt ${3} ]]; then
-		
 			log_msg="$(date) : $1 : $3 : $4 : $5"
 			echo ${log_msg} >> ${init_log}
 			if [[ "${logConfig_init_logEcho}" = "on" ]]; then
@@ -51,12 +41,11 @@ log_it() {
 					echo ${log_msg}
 				fi
 			fi
-    	else
+		else
 			echo "Error: ${__function_name} : Unkwonk log type: ${option}"
-		    exit 1            
+			exit 1
 		fi
 	fi
-	
 }
 
 echo "tets2"

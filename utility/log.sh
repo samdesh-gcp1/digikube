37e2d12256261c91623f1ca6c7bd9551250d5d55
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-09 17:01:14 +0530

    Update log.sh

--- utility/log.sh
+++ utility/log.sh
@@ -3,11 +3,21 @@
 
 local base_dir=~/
 local log_dir=${base_dir}digikube-logs/
+local digi_dir=${base_dir}digikube/
+local log_config=${digi_dir}config/log-config.yaml
 local init_log=${log_dir}digikube-init.log
 local installer_log=${log_dir}digikube-installer.log
 
-. 
+. ${digi_dir}utility/general.sh
+eval $(parse_yaml $log_config )
 
+#Critical=4
+#Error=3
+#Warning=2
+#Information=1
+#Debug=0
+
+	
 function log_it {
     
     local __function_name="utility/log_it"
@@ -19,16 +29,30 @@ function log_it {
 
     option="${2}"
     case ${option} in 
-	    "init")         __log=${init_log}
+	
+	    "init")         if [[ $log-config_init_log-level -lt $3 ]]; then
+							log_msg="$(date) : $1 : $3 : $4 : $5"
+							echo $log_msg >> ${init_log}
+							if [[ $log-config_init_log-echo == "on" ]]; then
+								echo $log_msg
+							fi
+						fi
                         ;;
-  	    "installer")	__log=${installer_log}
+						
+  	    "installer")	if [[ $log-config_installer_log-level -lt $3 ]]; then
+							log_msg="$(date) : $1 : $3 : $4 : $5"
+							echo $log_msg >> ${installer_log}
+							if [[ $log-config_installer_log-echo == "on" ]]; then
+								echo $log_msg
+							fi
+						fi
+                        ;;
+						
+		__log=${installer_log}
                         ;;
 	    *)	            echo "Error: ($__function_name): Unkwonk log type: ${option}"
 		                exit 1
 		                ;;
     esac
-    
-    local log_msg="$(date) : $1 : $3 : $4 : $5"
-        >> ${__log}
-    
+      
 }

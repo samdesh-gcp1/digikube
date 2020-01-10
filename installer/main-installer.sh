b2d1a0b73bd90fd9e5c9b962a51b7f573d5c8e5d
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-10 19:23:37 +0530

    Update main-installer.sh

--- installer/main-installer.sh
+++ installer/main-installer.sh
@@ -3,6 +3,9 @@
 
 base_dir=~/
 digi_dir=${base_dir}digikube/
-kubectl_installer=${digi_dir}installer/kubectl-installer.sh
 
+kubectl_installer=${digi_dir}installer/kubectl-installer.sh
 ${kubectl_installer}
+
+kops_installer=${digi_dir}installer/kops-installer.sh
+${kops_installer}

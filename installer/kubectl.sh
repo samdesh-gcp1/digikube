caa8bde2464ea2bd308243a4d99851af6ee57824
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-10 14:47:29 +0530

    Update kubectl.sh

--- installer/kubectl.sh
+++ installer/kubectl.sh
@@ -10,7 +10,7 @@ digi_dir=${base_dir}digikube/
 . ${digi_dir}utility/log.sh
 
 digikube_config=${digi_dir}config/digikube-config.yaml
-eval $(parse_yaml ${digikube_config} "__config" )
+eval $(parse_yaml ${digikube_config} "__config_" )
 
 log_it "${__function_name}" "installer" 1 "0000" "Started the kubectl installation process"
 

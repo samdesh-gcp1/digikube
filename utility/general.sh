7a8e7ef0ee6e57ded838cdd2044c6809bb83818e
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-13 16:16:19 +0530

    Update general.sh

--- utility/general.sh
+++ utility/general.sh
@@ -44,7 +44,8 @@ function parse_yaml {
                 
         local prefix=$2
         local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-        sed -ne "s|^\($s\):|\1|" \
+        
+        eval(sed -ne "s|^\($s\):|\1|" \
                 -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
                 -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
         awk -F$fs '{
@@ -53,9 +54,9 @@ function parse_yaml {
                 for (i in vname) {if (i > indent) {delete vname[i]}}
                 if (length($3) > 0) {
                         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-                        printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+                        printf("%s%s%s=\"%s\"\n", "export '$prefix'",vn, $2, $3);
                 }
-        }'
+        }')
 }
 
 function replace_substring {

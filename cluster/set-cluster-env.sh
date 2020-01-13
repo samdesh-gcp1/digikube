8ae07c6076bb0c22af22079a921b2d053ff43044
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-13 17:24:42 +0530

    Update set-cluster-env.sh

--- cluster/set-cluster-env.sh
+++ cluster/set-cluster-env.sh
@@ -8,17 +8,19 @@ digi_dir=${base_dir}digikube/
 
 . ${digi_dir}utility/general.sh
 . ${digi_dir}utility/log.sh
+. ${digi_dir}common/digikube-config.sh
 
 log_it "${__function_name}" "cluster" "DEBUG" "2210" "Setting the cluster environment variables"
 
-digikube_config=${digi_dir}config/digikube-config.yaml
-eval $(parse_yaml ${digikube_config} "__config_" )
-
-export KOPS_FEATURE_FLAGS=${__config_cluster_kops_featureFlags}
-export KOPS_CLOUD=${__config_cloud_provider}
-if [[ 
-export KOPS_PROJECT=`gcloud config get-value project`
-export KOPS_VPC=digikube-vpc
+validate-digikube-config
+if [[ $? -gt 0 ]]; then
+    log_it "${__function_name}" "cluster" "ERR" "2210" "Unreconsiable mismatch between existing environment and configuration.  Exiting."
+    exit 1
+else
+    export KOPS_FEATURE_FLAGS=$(get-config-value "cluster.kops.featureFlags")
+    export KOPS_CLOUD=$(get-config-value "cloud.provider")
+    export KOPS_PROJECT=$(get-config-value "cloud.provider.project")
+    export KOPS_VPC=digikube-vpc
 export KOPS_ENV=dev1
 export KOPS_CLUSTER_NAME=${KOPS_PROJECT}-${KOPS_ENV}.k8s.local
 export KOPS_STATE_STORE=gs://${KOPS_PROJECT}-bucket/

eb282d26791fe7a0673babc4fab752b9823bcb4e
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-13 19:35:50 +0530

    Update kubectl-installer.sh

--- installer/kubectl-installer.sh
+++ installer/kubectl-installer.sh
@@ -8,13 +8,15 @@ digi_dir=${base_dir}digikube/
 
 . ${digi_dir}utility/general.sh
 . ${digi_dir}utility/log.sh
+. ${digi_dir}common/digikube-config.sh
 
-digikube_config=${digi_dir}config/digikube-config.yaml
-parse_yaml "${digikube_config}" "__config_"
+#digikube_config=${digi_dir}config/digikube-config.yaml
+#parse_yaml "${digikube_config}" "__config_"
 
 log_it "${__function_name}" "installer" "INFO" "1110" "Started the kubectl installation process"
 
-kubectl_download_version=${__config_component_kubectl_version}
+#kubectl_download_version=${__config_component_kubectl_version}
+kubectl_download_version=$(get-config-value "component.kubectl.version")
 log_it "${__function_name}" "installer" "DEBUG" "1115" "Target version of kubectl to be installed is: $kubectl_download_version"
 
 kubectl_existing_version=""

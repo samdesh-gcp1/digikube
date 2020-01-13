e79b9ee5ce54273120cc800be1b56ad283fe17bb
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-13 18:57:13 +0530

    Update general.sh

--- utility/general.sh
+++ utility/general.sh
@@ -39,28 +39,27 @@ function download_file {
 }
 
 function parse_yaml_temp {
-
-        local __function_name="parse_yaml_temp"
-                
-        local prefix=$2
-        local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
-        
-        sed -ne "s|^\($s\):|\1|" \
-                -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-                -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-        awk -F$fs '{
-                indent = length($1)/2;
-                vname[indent] = $2;
-                for (i in vname) {if (i > indent) {delete vname[i]}}
-                if (length($3) > 0) {
-                        vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-                        printf("%s%s%s=\"%s\"\n", "export '$prefix'",vn, $2, $3);
-                }
-        }'
+   local __function_name="parse_yaml_temp"
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
 }
 
 function parse_yaml {
-      eval $(parse_yaml_temp $1 $2)
+      echo $1
+      echo $2
+      eval $(parse_yaml_temp "$1" "$2")
 }
 
 function replace_substring {

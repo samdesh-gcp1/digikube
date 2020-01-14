7a49642cd63e38cfd558b074a28559fa3191f9ec
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-14 14:58:26 +0530

    Update digikube-config.sh

--- common/digikube-config.sh
+++ common/digikube-config.sh
@@ -63,12 +63,11 @@ function get-config-value {
     log_it "${__function_name}" "installer" "DEBUG" "2110" "Config name : ${config_name}"
     if [[ -z ${config_name} ]]; then
         log_it "${__function_name}" "installer" "ERR" "2110" "Invalid config $1"
-    #    echo "ERR"
+        echo ""
     #else
-         echo "${config_name)"
-    #    local config_value="${!config_name)"
-    #    log_it "${__function_name}" "installer" "DEBUG" "2110" "Config value : ${config_value}"
-    #    echo "${config_value}"
+        local config_value="${!config_name}"
+        log_it "${__function_name}" "installer" "DEBUG" "2110" "Config value : ${config_value}"
+        echo "${config_value}"
     fi
     echo "111"
 }

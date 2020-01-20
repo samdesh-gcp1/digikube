c76baea0b83848932107e241603c9f09f729b348
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-20 20:47:55 +0530

    Update kops-installer.sh

--- installer/kops-installer.sh
+++ installer/kops-installer.sh
@@ -56,51 +56,51 @@ if [[ -f ${kops_existing_path} ]]; then
         fi
     fi
 
-else
-
-    #kops not available.  Download and install
-    kops_local_path=${__config_component_kops_localPath}
-    sudo cp --preserve ${kops_shell_file} ${kops_local_path}
-    _exit_code=$?
-    if [[ $? -gt 0 ]]; then
-      log_it "${__function_name}" "installer" "ERR" "1270" "Error while copying kops shell to ${kops_local_path}"
-      exit 1
-    else        
-      if [[ -f "${kops_local_path}" ]]; then
-        log_it "${__function_name}" "installer" "DEBUG" "1274" "Moved kops shell to ${kops_local_path}"
-        
-        chmod +x ${kops_local_path}
-        if [[ $? -gt 0 ]]; then
-            log_it "${__function_name}" "installer" "ERR" "1255" "Error while changing the file perimissions for the kops shell"
-            exit 1
-        else
-            log_it "${__function_name}" "installer" "DEBUG" "1260" "Changed the access permission of kops shell"
-        fi
-      else
-        log_it "${__function_name}" "installer" "ERR" "1278" "Error while moving kops shell to ${kops_local_path}"
-        exit 1
-      fi
-    fi
-        
-    #Check if kops shell is in path
-    kops_new_path=$(which kops)
-    if [[ $? -gt 0 ]]; then
-        log_it "${__function_name}" "installer" "ERR" "1280" "Error while checking if kops shell is in path"
-        exit 1
-    fi
-    if [[ "${kops_new_path}" = "${kops_local_path}" ]]; then
-        log_it "${__function_name}" "installer" "DEBUG" "1282" "kops shell is in path"
-    else 
-        log_it "${__function_name}" "installer" "ERR" "1284" "Different kops shell is available in path."
-        exit 1
-    fi
-        
-    kops_inpath_version=$(kops version | cut -d " " -f 2)
-    if [[ "${kops_inpath_version}" = "${kops_download_version}" ]]; then
-        log_it "${__function_name}" "installer" "DEBUG" "1286" "kops binary in path has version: $kops_download_version"
-        log_it "${__function_name}" "installer" "INFO" "1288" "Successfully installed kops version: $kops_download_version"
-    else
-        log_it "${__function_name}" "installer" "ERR" "1290" "Incorrect version of kops shell in path.  Expected version: ${kops_download_version} Actual version: ${kops_inpath_version}"
-    fi
-      
 fi
+
+#kops not available.  Download and install
+kops_local_path=${__config_component_kops_localPath}
+sudo cp --preserve ${kops_shell_file} ${kops_local_path}
+_exit_code=$?
+if [[ $? -gt 0 ]]; then
+  log_it "${__function_name}" "installer" "ERR" "1270" "Error while copying kops shell to ${kops_local_path}"
+  exit 1
+else        
+  if [[ -f "${kops_local_path}" ]]; then
+    log_it "${__function_name}" "installer" "DEBUG" "1274" "Moved kops shell to ${kops_local_path}"
+    
+    chmod +x ${kops_local_path}
+    if [[ $? -gt 0 ]]; then
+        log_it "${__function_name}" "installer" "ERR" "1255" "Error while changing the file perimissions for the kops shell"
+        exit 1
+    else
+        log_it "${__function_name}" "installer" "DEBUG" "1260" "Changed the access permission of kops shell"
+    fi
+  else
+    log_it "${__function_name}" "installer" "ERR" "1278" "Error while moving kops shell to ${kops_local_path}"
+    exit 1
+  fi
+fi
+        
+#Check if kops shell is in path
+kops_new_path=$(which kops)
+if [[ $? -gt 0 ]]; then
+    log_it "${__function_name}" "installer" "ERR" "1280" "Error while checking if kops shell is in path"
+    exit 1
+fi
+if [[ "${kops_new_path}" = "${kops_local_path}" ]]; then
+    log_it "${__function_name}" "installer" "DEBUG" "1282" "kops shell is in path"
+else 
+    log_it "${__function_name}" "installer" "ERR" "1284" "Different kops shell is available in path."
+    exit 1
+fi
+        
+kops_inpath_version=$(kops version | cut -d " " -f 2)
+if [[ "${kops_inpath_version}" = "${kops_download_version}" ]]; then
+    log_it "${__function_name}" "installer" "DEBUG" "1286" "kops binary in path has version: $kops_download_version"
+    log_it "${__function_name}" "installer" "INFO" "1288" "Successfully installed kops version: $kops_download_version"
+else
+    log_it "${__function_name}" "installer" "ERR" "1290" "Incorrect version of kops shell in path.  Expected version: ${kops_download_version} Actual version: ${kops_inpath_version}"
+fi
+     
+

ada59ba6dc881f17f6403a05075fd071a1e86d14
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-21 20:24:12 +0530

    Update helm-om.sh

--- cluster/sys-apps-om/helm-om.sh
+++ cluster/sys-apps-om/helm-om.sh
@@ -1,12 +1,11 @@
 function deploy {
-
+    echo "Command 'helm deploy' not yet implemented"
 }
 
 function delete {
-
+    echo "Command 'helm delete' not yet implemented"
 }
 
 function update {
-
-
+    echo "Command 'helm update' not yet implemented"
 }

9bc9ae81c14fc2662ecfc1d07a68f7be0f5249de
Sameer Deshpande <samdesh.gcp1@gmail.com>
2020-01-30 20:36:02 +0530

    Update cloud.sh

--- utility/cloud.sh
+++ utility/cloud.sh
@@ -4,8 +4,9 @@
 function get-cloud-project {
 
 if [[ "$1" == "gce" ]]; then
-    echo "$(who am i)"
-    #echo "$(gcloud info |tr -d '[]' | awk '/project:/ {print $2}')"
+    #echo "$(who am i)"
+    
+    echo "$(/snap/bin/gcloud info |tr -d '[]' | awk '/project:/ {print $2}')"
 fi
 
 }
